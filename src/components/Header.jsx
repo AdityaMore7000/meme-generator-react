7c20860f717261b30ffd0c912f6f827a31162e99
Aditya More <99179330+AdityaMore7000@users.noreply.github.com>
2023-12-16 04:42:56 +0000

    change favicon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
     <header className="header flex text-white py-2 items-center gap-2 bg-gradient-to-r mx-3 my-2 rounded-md from-purple-800 to-purple-950">
         <img src={face} alt="troll face" className='w-[100px] ml-2'/>
         <h2 className='text-xl font-bold'>
-            Meme Generator
+            Memes Generator
         </h2>
         <p className='absolute right-10 text-lg'>React course-Project 3</p>
     </header>
